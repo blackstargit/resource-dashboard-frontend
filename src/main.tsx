0b7c5334eebe99ddfee7965e0e8861341d3d8b9f
Black Star <blackst1rmail@gmail.com>
2026-02-13 06:54:51 +0000

    refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-import './style.css'
-import './styles.css'
+import './styles/index.css'
+import './styles/style.css'
+import './styles/styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
